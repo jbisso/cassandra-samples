c9b2e92ae937fe62259b03b8a0d46d5b88d6cfe0
Jim Bisso <jim@datastax.com>
2013-06-06 14:55:56 -0700

    Add the C# project.

---
+++ csharp/SimpleCassandraClient/SimpleCassandraClient/BoundStatementsClient.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CassandraExamples
+{
+    public class BoundStatementsClient : SimpleCassandraClient
+    {
+        public BoundStatementsClient()
+        {
+        }
+
+        public void LoadData()
+        {
+            PreparedStatement statement = getSession().prepare(
+                  "INSERT INTO songs " +
+                  "(id, title, album, artist, tags) " +
+                  "VALUES (?, ?, ?, ?, ?);");
+            BoundStatement boundStatement = new BoundStatement(statement);
+            Set<String> tags = new HashSet<String>();
+            tags.add("jazz");
+            tags.add("2013");
+            getSession().execute(boundStatement.bind(
+                  UUID.fromString("756716f7-2e54-4715-9f00-91dcbea6cf50"),
+                  "La Petite Tonkinoise'",
+                  "Bye Bye Blackbird'",
+                  "Joséphine Baker",
+                  tags));
+            tags = new HashSet<String>();
+            tags.add("1996");
+            tags.add("1996");
+            tags.add("birds");
+            getSession().execute(boundStatement.bind(
+                  UUID.fromString("f6071e72-48ec-4fcb-bf3e-379c8a696488"),
+                  "Die Mösch",
+                  "In Gold'",
+                  "Willi Ostermann",
+                  tags));
+            tags = new HashSet<String>();
+            tags.add("1996");
+            tags.add("1996");
+            tags.add("birds");
+            getSession().execute(boundStatement.bind(
+                  UUID.fromString("fbdf82ed-0063-4796-9c7c-a3d4f47b4b25"),
+                  "Memo From Turner",
+                  "Performance",
+                  "Mick Jager",
+                  tags));
+            // playlists table
+            statement = getSession().prepare(
+                  "INSERT INTO playlists " +
+                  "(id, song_id, title, album, artist) " +
+                  "VALUES (?, ?, ?, ?, ?);");
+            boundStatement = new BoundStatement(statement);
+            getSession().execute(boundStatement.bind(
+                  UUID.fromString("2cc9ccb7-6221-4ccb-8387-f22b6a1b354d"),
+                  UUID.fromString("756716f7-2e54-4715-9f00-91dcbea6cf50"),
+                  "La Petite Tonkinoise",
+                  "Bye Bye Blackbird",
+                  "Joséphine Baker"));
+            getSession().execute(boundStatement.bind(
+                  UUID.fromString("2cc9ccb7-6221-4ccb-8387-f22b6a1b354d"),
+                  UUID.fromString("f6071e72-48ec-4fcb-bf3e-379c8a696488"),
+                  "Die Mösch",
+                  "In Gold",
+                  "Willi Ostermann"));
+            getSession().execute(boundStatement.bind(
+                  UUID.fromString("3fd2bedf-a8c8-455a-a462-0cd3a4353c54"),
+                  UUID.fromString("fbdf82ed-0063-4796-9c7c-a3d4f47b4b25"),
+                  "Memo From Turner",
+                  "Performance",
+                  "Mick Jager"));
+        }
+
+        static void Main(string[] args)
+        {
+            BoundStatementsClient client = new BoundStatementsClient();
+            client.Connect("127.0.0.1");
+            client.CreateSchema();
+            client.LoadData();
+            client.QuerySchema();
+            client.UpdateSchema();
+            client.DropSchema("simplex");
+            client.Close();
+        }
+    }
+}
---
+++ csharp/SimpleCassandraClient/SimpleCassandraClient/SimpleCassandraClient.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Cassandra;
+
+namespace CassandraExamples
+{
+    class SimpleCassandraClient
+    {
+        public Session session { get; private set; }
+
+        public SimpleCassandraClient()
+        {
+        }
+
+        public void Connect(String node)
+        {
+            Cluster cluster = Cluster.Builder()
+                   .AddContactPoint(node)
+                   .Build();
+            session = cluster.Connect();
+            Metadata metadata = cluster.Metadata;
+            Console.WriteLine("Connected to cluster: " + metadata.GetClusterName().ToString());
+        }
+
+        public void CreateSchema()
+        {
+            session.Execute(
+                "CREATE KEYSPACE simplex WITH replication " + 
+                "= {'class':'SimpleStrategy', 'replication_factor':1};");
+            // create songs and playlist tables
+            session.Execute(
+                "CREATE TABLE simplex.songs (" +
+                        "id uuid PRIMARY KEY," + 
+                        "title text," + 
+                        "album text," + 
+                        "artist text," + 
+                        "tags set<text>," + 
+                        "data blob" + 
+                    ");");
+            session.Execute(
+                "CREATE TABLE simplex.playlists (" +
+                        "id uuid," +
+                        "title text," +
+                        "album text, " + 
+                        "artist text," +
+                        "song_id uuid," +
+                        "PRIMARY KEY (id, title, album, artist)" +
+                        ");");
+            Console.WriteLine("Simplex keyspace and schema created.");
+        }
+
+        public void LoadData()
+        {
+            // insert data in the tables
+            session.Execute(
+                "INSERT INTO simplex.songs (id, title, album, artist, tags) " +
+                "VALUES (" +
+                    "756716f7-2e54-4715-9f00-91dcbea6cf50," +
+                    "'La Petite Tonkinoise'," +
+                    "'Bye Bye Blackbird'," +
+                    "'Joséphine Baker'," +
+                    "{'jazz', '2013'})" +
+                ";");
+            session.Execute(
+                "INSERT INTO simplex.songs (id, title, album, artist, tags) " +
+                "VALUES (" +
+                    "f6071e72-48ec-4fcb-bf3e-379c8a696488," +
+                    "'Die Mösch'," +
+                    "'In Gold'," +
+                    "'Willi Ostermann'," +
+                    "{'kölsch', '1996', 'birds'}" +
+                ");");
+            session.Execute(
+                "INSERT INTO simplex.songs (id, title, album, artist, tags) " +
+                "VALUES (" +
+                    "fbdf82ed-0063-4796-9c7c-a3d4f47b4b25," +
+                    "'Memo From Turner'," +
+                    "'Performance'," +
+                    "'Mick Jager'," +
+                    "{'soundtrack', '1991'}" +
+                ");");
+            session.Execute(
+                "INSERT INTO simplex.playlists (id, song_id, title, album, artist) " +
+                "VALUES (" +
+                    "2cc9ccb7-6221-4ccb-8387-f22b6a1b354d," +
+                    "756716f7-2e54-4715-9f00-91dcbea6cf50," +
+                    "'La Petite Tonkinoise'," +
+                    "'Bye Bye Blackbird'," +
+                    "'Joséphine Baker'" +
+                ");");
+            session.Execute(
+                "INSERT INTO simplex.playlists (id, song_id, title, album, artist) " +
+                "VALUES (" +
+                    "2cc9ccb7-6221-4ccb-8387-f22b6a1b354d," +
+                    "f6071e72-48ec-4fcb-bf3e-379c8a696488," +
+                    "'Die Mösch'," +
+                    "'In Gold'," +
+                    "'Willi Ostermann'" +
+                ");");
+            session.Execute(
+                "INSERT INTO simplex.playlists (id, song_id, title, album, artist) " +
+                "VALUES (" +
+                    "3fd2bedf-a8c8-455a-a462-0cd3a4353c54," +
+                    "fbdf82ed-0063-4796-9c7c-a3d4f47b4b25," +
+                    "'Memo From Turner'," +
+                    "'Performance'," +
+                    "'Mick Jager'" +
+                ");");
+            Console.WriteLine("Data loaded.");
+        }
+
+        public void QuerySchema()
+        {
+            session.Execute(
+                "UPDATE simplex.songs " +
+                "SET tags = tags + { 'entre-deux-guerre' } " +
+                "WHERE id = 756716f7-2e54-4715-9f00-91dcbea6cf50;");
+      
+            CqlRowSet results = session.Execute(
+                "SELECT * FROM simplex.songs " +
+                "WHERE id = 756716f7-2e54-4715-9f00-91dcbea6cf50;");
+      
+            Console.WriteLine( String.Format("{0, -30}\t{1, -20}\t{2, -20}\t{3, -30}", 
+                "title", "album", "artist", "tags") );
+            Console.WriteLine("-------------------------------+-----------------------+--------------------+-------------------------------");
+            foreach (CqlRow row in results.GetRows())
+            {
+                Console.WriteLine( String.Format("{0, -30}\t{1, -20}\t{2, -20}\t{3}",
+                    row.GetValue<String>("title"), row.GetValue<String>("album"),
+                    row.GetValue<String>("artist"), row.GetValue<List<String>>("tags").ToString()) );
+            }
+        }
+
+        public void UpdateSchema() {
+            session.Execute(
+                "UPDATE simplex.songs " +
+                "SET tags = tags + { 'entre-deux-guerre' } " +
+                "WHERE id = 756716f7-2e54-4715-9f00-91dcbea6cf50;");
+      
+            CqlRowSet results = session.Execute(
+                "SELECT * FROM simplex.songs " +
+                "WHERE id = 756716f7-2e54-4715-9f00-91dcbea6cf50;");
+      
+            Console.WriteLine( String.Format("{0, -30}\t{1, -20}\t{2, -20}", "title", "album", "artist", "tags");
+            Console.WriteLine("-------------------------------+-----------------------+--------------------+-------------------------------"));
+            foreach (CqlRow row in results.GetRows()) {
+                Console.WriteLine( String.Format("{0, -30}\t{1, -20}\t{2, -20}", 
+                    row.GetValue<String>("title"), row.GetValue<String>("album"), 
+                    row.GetValue<String>("artist"), row.GetValue<List<String>>("tags")));
+            }
+        }
+
+        public void DropSchema(String keyspace)
+        {
+            session.Execute("DROP KEYSPACE " + keyspace + ";");
+        }
+
+        public void Close()
+        {
+            session.Cluster.Shutdown();
+            session.Dispose();
+        }
+
+        static void Main(string[] args)
+        {
+            SimpleCassandraClient client = new SimpleCassandraClient();
+            client.Connect("127.0.0.1");
+            client.CreateSchema();
+            client.LoadData();
+            client.QuerySchema();
+            Console.WriteLine("Hit any key to quit.");
+            Console.ReadKey(false);
+            client.DropSchema("simplex");
+            client.Close();
+        }
+    }
+}
