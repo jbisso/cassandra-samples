6f88c48f4e0b83b66370f96f5b8537703c50e319
Jim Bisso <jim@datastax.com>
2013-06-18 10:04:12 -0700

    Refactored the SimpleClient solution.

---
+++ csharp/SimpleClient/CassandraApplication/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CassandraApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //SimpleClient client = new SimpleClient();
+            BoundStatementsClient client = new BoundStatementsClient();
+            client.Connect("127.0.0.1");
+            client.CreateSchema();
+            client.LoadData();
+            client.QuerySchema();
+            Console.WriteLine("Hit any key to quit.");
+            Console.ReadKey(true);
+            client.DropSchema("simplex");
+            client.Close();
+        }
+    }
+}
