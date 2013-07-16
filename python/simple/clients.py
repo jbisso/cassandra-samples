3ec856c80c174ffef26ed1ad4f4262447a71c828
Jim Bisso <jim@datastax.com>
2013-07-15 17:00:43 -0700

    Added bound statements example and async one, too. The latter is not working yet.

--- python/simple/clients.py
+++ python/simple/clients.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from cassandra.cluster import Cluster
+from cassandra.query import PreparedStatement
 import logging
 
 class SimpleClient:
@@ -11,15 +12,19 @@ class SimpleClient:
 
     session = None
 
+    @classmethod
     def connect(self, nodes):
         cluster = Cluster(nodes)
         metadata = cluster.metadata
         self.session = cluster.connect()
         print('Connected to cluster: ', metadata.cluster_name)
 
+    @classmethod
     def create_schema(self):
-        self.session.execute('CREATE KEYSPACE simplex WITH replication ' + 
-            '= {\'class\':\'SimpleStrategy\', \'replication_factor\':3};')
+        self.session.execute("""
+            CREATE KEYSPACE simplex WITH replication
+                = {'class':'SimpleStrategy', 'replication_factor':3};
+        """)
         self.session.execute("""
             CREATE TABLE simplex.songs (
                 id uuid PRIMARY KEY,
@@ -42,6 +47,7 @@ class SimpleClient:
         """)
         print('Simplex keyspace and schema created.')
 
+    @classmethod
     def load_data(self):
         self.session.execute("""
             INSERT INTO simplex.songs (id, title, album, artist, tags)
@@ -105,6 +111,7 @@ class SimpleClient:
         """)
         print('Data loaded.')
 
+    @classmethod
     def query_schema(self):
         results = self.session.execute("""
             SELECT * FROM simplex.playlists
@@ -116,6 +123,7 @@ class SimpleClient:
         for row in results:
             print("%-30s\t%-20s\t%-20s" % (row.title, row.album, row.artist) )
 
+    @classmethod
     def update_schema(self):
         self.session.execute("""
             UPDATE simplex.songs
@@ -133,23 +141,109 @@ class SimpleClient:
             print("%-30s\t%-20s\t%-20s%-30s" %
                 (row.title, row.album, row.artist, row.tags) )
 
+    @classmethod
     def drop_schema(self, keyspace):
         self.session.execute("DROP keyspace " + keyspace + ";")
         print("Dropped keyspace " + keyspace)
 
+    @classmethod
     def close(self):
         self.session.cluster.shutdown()
         self.session.shutdown()
 
+class BoundStatementsClient(SimpleClient):
+    @classmethod    
+    def loadData(self):
+        """
+        Overload SimpleClient's method to use bound t=statements.
+        """
+        statement = self.session.prepare("""
+            INSERT INTO songs
+            (id, title, album, artist, tags)
+            VALUES (?, ?, ?, ?, ?);
+        """)
+        boundStatement = BoundStatement(statement)
+        tags = set(['jazz', '2013'])
+        self.session.execute(boundStatement.bind(
+            UUID("756716f7-2e54-4715-9f00-91dcbea6cf50"),
+            "La Petite Tonkinoise",
+            "Bye Bye Blackbird",
+            "Joséphine Baker",
+            tags )
+        )
+        tags = set(['1996', 'birds'])
+        self.session.execute(boundStatement.bind(
+            UUID("f6071e72-48ec-4fcb-bf3e-379c8a696488"),
+            "Die Mösch",
+            "In Gold'", 
+            "Willi Ostermann",
+            tags )
+        )
+        tags = set(['1970', 'soundtrack'])
+        self.session.execute(boundStatement.bind(
+            UUID("fbdf82ed-0063-4796-9c7c-a3d4f47b4b25"),
+            "Memo From Turner",
+            "Performance",
+            "Mick Jager",
+            tags )
+        )
+        statement = getSession().prepare("""
+            INSERT INTO playlists
+            (id, song_id, title, album, artist)
+            VALUES (?, ?, ?, ?, ?);
+        """)
+        boundStatement = BoundStatement(statement)
+        getSession().execute(boundStatement.bind(
+            UUID("2cc9ccb7-6221-4ccb-8387-f22b6a1b354d"),
+            UUID("756716f7-2e54-4715-9f00-91dcbea6cf50"),
+            "La Petite Tonkinoise",
+            "Bye Bye Blackbird",
+            "Joséphine Baker") 
+        )
+        getSession().execute(boundStatement.bind(
+            UUID("2cc9ccb7-6221-4ccb-8387-f22b6a1b354d"),
+            UUID("f6071e72-48ec-4fcb-bf3e-379c8a696488"),
+            "Die Mösch",
+            "In Gold",
+            "Willi Ostermann")
+        )
+        getSession().execute(boundStatement.bind(
+            UUID("3fd2bedf-a8c8-455a-a462-0cd3a4353c54"),
+            UUID("fbdf82ed-0063-4796-9c7c-a3d4f47b4b25"),
+            "Memo From Turner",
+            "Performance",
+            "Mick Jager")
+        )
 
+# callback functions for the AsynchronousExample class
+
+def print_errors(errors):
+    print(errors)
+    
+def print_results(results):
+    print("%-30s\t%-20s\t%-20s%-30s\n%s" %
+        ("title", "album", "artist",
+            "tags", "-------------------------------+-----------------------+--------------------+-------------------------------") )
+    for row in results:
+        print("%-30s\t%-20s\t%-20s%-30s" %
+            (row.title, row.album, row.artist, row.tags) )
+
+
+class AsynchronousExample(SimpleClient):
+    @classmethod
+    def query_schema(self):
+        future_results =  self.session.execute_async("SELECT * FROM simplex.songs;")
+        future_results.add_callbacks(print_results, print_errors)
+            
 # 
 
 def main():
     logging.basicConfig()
-    client = SimpleClient()
+    # client = SimpleClient()
+    # client = BoundStatementsClient()
+    client = AsynchronousExample()
     client.connect(['127.0.0.1'])
     client.create_schema()
-    client.load_data()
     client.query_schema()
     client.update_schema()
     client.drop_schema("simplex")
